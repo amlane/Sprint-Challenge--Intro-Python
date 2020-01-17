700960289194763df206924adb15ee2af31c6e88
amlane <twelvexstring@gmail.com>
2020-01-17 11:35:15 -0600

    city reader MVP completed, some of stretch done but test not passing

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,11 +1,22 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import csv
+
+
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __repr__(self):
+        return f"{self.name}({self.lat}, {self.lon})"
 
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -16,12 +27,21 @@
 # should not be loaded into a City object.
 cities = []
 
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+    with open("cities.csv") as citiesfile:
+        line_counter = 0
+        file_reader = csv.reader(citiesfile, delimiter=",")
+        for line in file_reader:
+            if line_counter == 0:
+                line_counter += 1
+            else:
+                cities.append(City(line[0], float(line[3]), float(line[4])))
     return cities
+
 
 cityreader(cities)
 
@@ -32,10 +52,10 @@ for c in cities:
 # STRETCH GOAL!
 #
 # Allow the user to input two points, each specified by latitude and longitude.
-# These points form the corners of a lat/lon square. Pass these latitude and 
+# These points form the corners of a lat/lon square. Pass these latitude and
 # longitude values as parameters to the `cityreader_stretch` function, along
 # with the `cities` list that holds all the City instances from the `cityreader`
-# function. This function should output all the cities that fall within the 
+# function. This function should output all the cities that fall within the
 # coordinate square.
 #
 # Be aware that the user could specify either a lower-left/upper-right pair of
@@ -60,12 +80,35 @@ for c in cities:
 
 # TODO Get latitude and longitude values from the user
 
+cmd1 = input("Please enter [lon1, lat1]: ")
+cmd2 = input("Please enter [lon2, lat2]: ")
+
+float1 = float(cmd1.split(",")[0])
+float2 = float(cmd1.split(",")[1])
+float3 = float(cmd2.split(",")[0])
+float4 = float(cmd2.split(",")[1])
+
+if float3 > float1:
+    float1 = float(cmd2.split(",")[0])
+    float3 = float(cmd1.split(",")[0])
+if float2 < float4:
+    float2 = float(cmd2.split(",")[1])
+    float4 = float(cmd1.split(",")[1])
+
+
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
+    # within will hold the cities that fall within the specified region
+    within = [c for c in cities if c.lat <= lat1 and c.lat >=
+              lat2 and c.lon <= lon1 and c.lon >= lon2]
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+    # TODO Ensure that the lat and lon valuse are all floats
+    # Go through each city and check to see if it falls within
+    # the specified coordinates.
 
-  return within
+    for w in within:
+        print(w)
+    return within
+
+
+print("++++++++++++++++++++++++++++++++++++++++++++++")
+cityreader_stretch(float1, float2, float3, float4, cities)
